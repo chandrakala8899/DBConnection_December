5505b3514bb960a8511841137873c21d926dd874
chandrakala <chandraklag688@gmail.com>
2024-12-19 16:22:56 +0530

    BeanLifeCycle

--- src/main/java/com/neoteric/dec_dbconnection/springbeanlifecycle/BeanTest.java
+++ src/main/java/com/neoteric/dec_dbconnection/springbeanlifecycle/BeanTest.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class BeanTest {
     public static void main(String args[]) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-     MyBean myBean =  context.getBean(MyBean.class);
-      myBean.setAdditionalProperty("  ");
+        MyBean myBean =  context.getBean(MyBean.class);
+      myBean.setAdditionalProperty(" Additional Property ");
       myBean.performTask();
       myBean.customDestroy();
       myBean.customInit();
